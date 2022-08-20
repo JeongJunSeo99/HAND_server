1b21622396a98e3c428708a661c2063f97c2b755
Ubuntu <ubuntu@ip-172-31-46-29.ap-northeast-2.compute.internal>
2022-08-20 20:20:06 +0000

    2

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+const mysql = require('mysql');  
+const port = 3001;
+
+var connection = mysql.createConnection({
+    host: "hand-mysql.c4v4p96dwz9h.ap-northeast-2.rds.amazonaws.com",
+    user: "wnstj",
+    database: "HANDdb",
+    password: "71898212asd!",
+    port: 3306
+});
+
+app
+    .use(bodyParser.urlencoded({extended: true}))
+    .use(bodyParser.json())
+    .use(express.json({ extended: false }))
+    .use(express.urlencoded({extended: true}))
+    //.use("/app_list", require("./routes/app_list"))
+    .listen(port, () => {console.log('Express is listening on port', port);})
